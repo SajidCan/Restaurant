fd2c3a3208d3e2d4b61227cec8542c3d787c6004
SajidCan <67926486+SajidCan@users.noreply.github.com>
2021-09-12 00:51:01 +0600

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,89 @@
+// $('.slicky').slick({
+//    autoplay:false,
+//    slidesToShow: 2,
+//    speed: 500,
+//    arrows: true,
+//    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+//    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+// });
 $('.slicky').slick({
-   autoplay:true,
-   slidesToShow: 3,
-   speed: 500,
-   arrows: true,
-   prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-   nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
-});
+  autoplay:false,
+  slidesToShow: 3,
+  speed: 500,
+  arrows: true,
+  responsive: [
+ {
+   breakpoint: 1024,
+   settings: {
+     slidesToShow: 3,
+     slidesToScroll: 3,
+     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 
+   }
+ },
+ {
+   breakpoint: 769,
+   settings: {
+     slidesToShow: 2,
+     slidesToScroll: 2,
+     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+   }
+ },
+ {
+   breakpoint: 590,
+   settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+   }
+ }
+],
+prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+});
 
 $('.review_slicky').slick({
-  infinite: true,
+  autoplay:false,
   slidesToShow: 2,
-  slidesToScroll: 1,
-  prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-  nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+  speed: 500,
+  arrows: true,
+  responsive: [
+ {
+   breakpoint: 1024,
+   settings: {
+     slidesToShow: 2,
+     slidesToScroll: 2,
+     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+   }
+ },
+ {
+   breakpoint: 769,
+   settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+   }
+ },
+ {
+   breakpoint: 480,
+   settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+   }
+ }
+],
+prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });
+
 
 var close = document.getElementsByClassName("closebtn");
 var i;
